77b979c8f84403ad35b5972ea98732353b5a3541
javines24 <javines2403@gmail.com>
2023-05-13 14:46:14 +0800

    Adding another problem solution

---
+++ Solutions/src/RemoveDuplicates3.java
@@ -0,0 +1,20 @@
+/**
+ * created by James Vincent Escartin on 5/13/23
+ **/
+public class RemoveDuplicates3 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2){
+            return nums.length;
+        }
+
+        int count = 2;
+        for(int i = 2; i < nums.length; i++){
+            if (nums[i] != nums[count - 2]){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
